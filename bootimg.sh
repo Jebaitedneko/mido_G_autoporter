ae7e3afc708d17f2c58ef15f926bfec8d290318a
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-05-29 02:26:35 +0530

    update scripts to repackage modified boot into zip

--- bootimg.sh
+++ bootimg.sh
@@ -1,8 +1,6 @@
 zipedit(){
-    
     curdir=$(pwd)
     unzip "$1" "boot.img" -d ./ 
-    
     #unpacking magic
     mv boot.img AIK-Linux
     cp Image.gz-dtb AIK-Linux
@@ -10,14 +8,15 @@ zipedit(){
     ./unpackimg.sh "boot.img"
     mv Image.gz-dtb split_img/ && cd split_img/
     rm boot.img-zImage && mv Image.gz-dtb boot.img-zImage
-    
-    #cmdline edits
+    #cmdline magic
     sed -i 's/enforcing/permissive/g' boot.img-cmdline
     sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' boot.img-cmdline
+    #repacking magic
     cd .. && ./repackimg.sh
-    mv image-new.img ../$1.$( date '+%F_%H:%M:%S' ).img
-    rm boot.img
-    cd $curdir
-    
+    mv image-new.img ../boot_G.img && rm boot.img
+    cd "$curdir"
+    zip -ur "$1" boot_G.img && rm boot_G.img
+    mv "$1" "${1}_PORTED.zip"
+    echo "\n\nPORTING FINISHED. FLASH '${1}_PORTED.zip' FOLLOWED BY PATCHES IN 'treble-fixes'\n\n"
 }
 zipedit $1
--- port.sh
+++ port.sh
@@ -1,5 +1,4 @@
 zipedit(){
-
     #updater script magic
     curdir=$(pwd)
     [ -e "/tmp/META-INF/com/google/android/updater-script" ] && echo "Removing old file" && rm /tmp/META-INF/com/google/android/updater-script
@@ -9,9 +8,9 @@ zipedit(){
     sed -i 's/\"mido\"/getprop\(\"ro.product.device\"\)/g' "META-INF/com/google/android/updater-script"
     sed -i 's/\"oxygen\"/getprop\(\"ro.product.device\"\)/g' "META-INF/com/google/android/updater-script"
     sed -i 's/cust/vendor/g' "META-INF/com/google/android/updater-script"
+    sed -i 's/boot\.img/boot\_G\.img/g' "META-INF/com/google/android/updater-script"
     zip --update "$curdir/$1" "META-INF/com/google/android/updater-script"
     cd "$curdir"
-    
     #unpacking magic
     mv boot.img AIK-Linux
     cp Image.gz-dtb AIK-Linux
@@ -19,13 +18,15 @@ zipedit(){
     ./unpackimg.sh "boot.img"
     mv Image.gz-dtb split_img/ && cd split_img/
     rm boot.img-zImage && mv Image.gz-dtb boot.img-zImage
-    
-    #cmdline edits
+    #cmdline magic
     sed -i 's/enforcing/permissive/g' boot.img-cmdline
     sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' boot.img-cmdline
+    #repacking magic
     cd .. && ./repackimg.sh
-    mv image-new.img ../$1.$( date '+%F_%H:%M:%S' ).img
-    rm boot.img
+    mv image-new.img ../boot_G.img && rm boot.img
     cd "$curdir"
+    zip -ur "$1" boot_G.img && rm boot_G.img
+    mv "$1" "${1}_PORTED.zip"
+    echo "\n\nPORTING FINISHED. FLASH '${1}_PORTED.zip' FOLLOWED BY PATCHES IN 'treble-fixes'\n\n"
 }
 zipedit $1
