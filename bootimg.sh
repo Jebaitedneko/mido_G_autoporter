016f686a8692795b4251de8df7bc88f86be138e0
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-06-05 19:46:32 +0530

    switched to google binaries and script rewrites

--- bootimg.sh
+++ bootimg.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 apt install zip unzip
-curdir=$(pwd)
-unzip $1 boot.img -d .
-mv boot.img AIK-Linux && cp Image.gz-dtb AIK-Linux && cd AIK-Linux
-./unpackimg.sh boot.img
-mv Image.gz-dtb split_img && cd split_img
-rm boot.img-zImage && mv Image.gz-dtb boot.img-zImage
-sed -i 's/enforcing/permissive/g' boot.img-cmdline
-sed -i 's/$/ androidboot\.selinux\=\permissive/' boot.img-cmdline
-sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' boot.img-cmdline
-cat boot.img-cmdline | sed 's/ /\n/g' | sort -u | tr '\n' ' ' > bcl && rm boot.img-cmdline && mv bcl boot.img-cmdline
-cd .. && ./repackimg.sh
-mv image-new.img ../boot_G.img && rm boot.img && cd $curdir
-zip -ur $1 boot_G.img && rm boot_G.img
-mv $1 ${1}_PORTED.zip && rm -rf META-INF
+mkdir -p boot
+unzip $1 $updater boot.img -d .
+./unpackbootimg -i boot.img -o boot && boot=boot/boot.img
+cp Image.gz-dtb $boot-zImage
+sed -i 's/enforcing/permissive/g' $boot-cmdline
+sed -i 's/$/ androidboot\.selinux\=\permissive/' $boot-cmdline
+sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' $boot-cmdline
+cat $boot-cmdline | sed 's/ /\n/g' | sort -u | tr '\n' ' ' > boot/bcl && rm $boot-cmdline && mv boot/bcl $boot-cmdline
+./mkbootimg --kernel $boot-zImage --ramdisk $boot-ramdisk.gz --cmdline "$(cat $boot-cmdline)" --base $(cat $boot-base) --pagesize $(cat $boot-pagesize) -o boot_G.img
+zip -ur $1 boot_G.img && rm boot.img && rm boot_G.img
+mv $1 ${1}_PORTED.zip && rm -rf boot
--- port.sh
+++ port.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 apt install zip unzip
-curdir=$(pwd)
 updater=META-INF/com/google/android/updater-script
-[ -e $updater ] && rm $updater
+mkdir -p boot
 [ `unzip -Z1 $1 | grep compatibility.zip` ] && zip -d $1 compatibility.zip
 unzip $1 $updater boot.img -d .
 sed -i 's/\"mido\"/getprop\(\"ro.product.device\"\)/g' $updater
@@ -10,15 +9,12 @@ sed -i 's/\"oxygen\"/getprop\(\"ro.product.device\"\)/g' $updater
 sed -i 's/cust/vendor/g' $updater
 sed -i 's/boot\.img/boot\_G\.img/g' $updater
 zip --update $1 $updater
-mv boot.img AIK-Linux && cp Image.gz-dtb AIK-Linux && cd AIK-Linux
-./unpackimg.sh boot.img
-mv Image.gz-dtb split_img && cd split_img
-rm boot.img-zImage && mv Image.gz-dtb boot.img-zImage
-sed -i 's/enforcing/permissive/g' boot.img-cmdline
-sed -i 's/$/ androidboot\.selinux\=\permissive/' boot.img-cmdline
-sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' boot.img-cmdline
-cat boot.img-cmdline | sed 's/ /\n/g' | sort -u | tr '\n' ' ' > bcl && rm boot.img-cmdline && mv bcl boot.img-cmdline
-cd .. && ./repackimg.sh
-mv image-new.img ../boot_G.img && rm boot.img && cd $curdir
-zip -ur $1 boot_G.img && rm boot_G.img
-mv $1 ${1}_PORTED.zip && rm -rf META-INF
+./unpackbootimg -i boot.img -o boot && boot=boot/boot.img
+cp Image.gz-dtb $boot-zImage
+sed -i 's/enforcing/permissive/g' $boot-cmdline
+sed -i 's/$/ androidboot\.selinux\=\permissive/' $boot-cmdline
+sed -i 's/$/ firmware_class\.path\=\/vendor\/firmware\_mnt\/image/' $boot-cmdline
+cat $boot-cmdline | sed 's/ /\n/g' | sort -u | tr '\n' ' ' > boot/bcl && rm $boot-cmdline && mv boot/bcl $boot-cmdline
+./mkbootimg --kernel $boot-zImage --ramdisk $boot-ramdisk.gz --cmdline "$(cat $boot-cmdline)" --base $(cat $boot-base) --pagesize $(cat $boot-pagesize) -o boot_G.img
+zip -ur $1 boot_G.img && rm boot.img && rm boot_G.img
+mv $1 ${1}_PORTED.zip && rm -rf META-INF && rm -rf boot
